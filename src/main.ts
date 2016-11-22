4b93840233dfa661a7f13cbefd820e86865db485
Pius Nyakoojo <pnyakoojo.16@gmail.com>
2016-11-21 23:32:21 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule, environment } from './app/';
 
+// Toggle production
 if (environment.production) {
   enableProdMode();
 }
